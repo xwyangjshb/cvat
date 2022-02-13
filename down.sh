fee171a32150d3d8a437c87ef4e0a6805b5ae3e2
andy <yangxw@betvsys.com>
2022-02-13 12:09:33 +0800

    修改适应国内的缓存镜像，测试nuclio YOLO3 标注能力

---
+++ down.sh
@@ -0,0 +1,6 @@
+echo down model 支持容器和服务
+# docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d
+
+docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml down
+
+
---
+++ start.sh
@@ -0,0 +1,6 @@
+echo 启动model 支持容器和服务
+docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml up -d
+
+# docker-compose -f docker-compose.yml -f components/serverless/docker-compose.serverless.yml down
+
+
